f0df85d19f7653a33d346166cf36e5f0bc8b3529
Md Habibur Rahman <shakibdshy@gmail.com>
2022-09-21 02:36:47 +0600

    Update CSS variable

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,48 @@
+import Head from 'next/head';
+import { ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import { useHotkeys, useLocalStorage } from "@mantine/hooks";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [colorScheme, setColorScheme] = useLocalStorage({
+    key: 'mantine-color-scheme',
+    defaultValue: 'dark',
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value) =>
+    setColorScheme(value || (colorScheme === 'light' ? 'dark' : 'light'));
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
+  return (
+    <>
+      <Head>
+        <title>Meetmax Social Networks</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            colorScheme,
+            components: {
+              Button: {
+                classNames: { root: 'button-root' },
+              },
+            },
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ColorSchemeProvider>
+
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
+import { Button } from '@mantine/core'
 import Head from 'next/head'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
-      
+      <Button>
+        Settings
+      </Button>
     </>
   )
 }
